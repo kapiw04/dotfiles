dbbc9982f471ff2caf0da4a21d087806a8fb3d98
kapiw04 <wojtowicz.kacper@gmail.com>
2025-08-19 19:18:37 +0000

    nixos (pre-rebuild): 2025-08-19 19:18:37

--- nixos/home.nix
+++ nixos/home.nix
@@ -2,37 +2,38 @@
   config,
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   home.username = "kapi";
   home.homeDirectory = "/home/kapi";
+  home.stateVersion = "25.05";
 
-  imports = [
-    inputs.zen-browser.homeModules.twilight
-  ];
+  imports = [inputs.zen-browser.homeModules.twilight];
 
-  home.packages = with pkgs; [gh go starship];
-  programs.git.enable = true;
+  home.packages = with pkgs; [gh go starship tree];
 
-  programs.neovim = {
-    enable = true;
-  };
+  programs = {
+    git.enable = true;
+    neovim.enable = true;
+    waybar.enable = true;
 
-  programs.zen-browser.enable = true;
-  programs.zen-browser.policies = {
-    DisableTelemetry = true;
-    DisableAppUpdate = true;
-    ExtensionSettings = {
-      "uBlock0@raymondhill.net" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        installation_mode = "force_installed";
-      };
-      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-        install_url = "https://addons.mozilla.org/firefox/downloads/file/4552124/bitwarden_password_manager-2025.7.1.xpi";
-        installation_mode = "force_installed";
+    zen-browser = {
+      enable = true;
+      policies = {
+        DisableTelemetry = true;
+        DisableAppUpdate = true;
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4552124/bitwarden_password_manager-2025.7.1.xpi";
+            installation_mode = "force_installed";
+          };
+        };
       };
     };
   };
-
-  home.stateVersion = "25.05"; # set once for new setups
 }
