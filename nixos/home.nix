d39eb536ee74922bf996b5ce06c2a50173ea0555
kapiw04 <wojtowicz.kacper@gmail.com>
2025-08-31 20:09:48 +0000

    nixos (pre-rebuild): 2025-08-31 20:09:48

--- nixos/home.nix
+++ nixos/home.nix
@@ -11,7 +11,7 @@
 
   imports = [inputs.zen-browser.homeModules.twilight];
 
-  home.packages = with pkgs; [flameshot gamescope unrar pixi gh go starship tree];
+  home.packages = with pkgs; [flameshot vulkan-tools libdecor libdecor-plugins gamescope unrar pixi gh go starship tree];
 
   programs = {
     git.enable = true;
